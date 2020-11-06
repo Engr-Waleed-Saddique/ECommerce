e9de10a15fb440c80e234e4f66dc36452fb18a55
Waleed Saddique <waleed.saddique.ch@gmail.com>
2020-11-07 00:11:52 +0500

    Adding Product Controller

---
+++ ECommerce.Services/ProductService.cs
@@ -0,0 +1,63 @@
+﻿
+using ECommerce.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ECommerce.Entities;
+
+namespace ECommerce.Services
+{
+    public class ProductService
+    {
+        public void SaveProduct(Product product)
+        {
+            using (var context= new CBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                //context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+
+                context.SaveChanges();
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var context = new CBContext())
+            {
+
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+
+                return context.Products.Find(ID);
+
+            }
+        }
+    }
+
+}
---
+++ ECommerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService productService = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable()
+        {
+            var products = productService.GetProducts();
+            return View(products);
+        }
+    }
+}
